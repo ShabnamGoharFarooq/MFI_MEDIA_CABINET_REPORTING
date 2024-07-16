38caf424b124974d40cd33c0acabb594ddbdf02c
shabnamfarooq <shabnam@homeeasy.com>
2024-07-16 07:53:17 -0700

    Your commit message

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-DATABASE = 'C:\\Users\\Home Easy\\Desktop\\My_database'
+DATABASE = 'My_database'
 def connect_db():
     conn = sqlite3.connect(DATABASE)
     conn.execute("PRAGMA foreign_keys = 1")  # Enable foreign key constraints

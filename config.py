48ad2c12a27a8a7f0c913a527a0716182a52dd2d
shabnamfarooq <shabnam@homeeasy.com>
2024-07-17 10:35:34 -0700

    Your commit message here

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ DATABASE = os.getenv('SQLITE_DATABASE_URL')
 
 if DATABASE is None:
     # Check if running in a deployment environment like Render
-    if os.getenv('RENDER'):
-        DATABASE = '/mnt/data/My_database.db'
+    if os.getenv('RENDER_EXTERNAL_HOSTNAME'):
+        DATABASE = '/mnt/data/My_database.sqlite'
     else:
-        DATABASE = 'My_database'
+        DATABASE = 'My_database.sqlite'
 
 def get_database_path():
     print(f"Using database path: {DATABASE}")  # Log the database path for verification

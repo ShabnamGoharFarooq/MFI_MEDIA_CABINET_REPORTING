66c05d299f9b072e8d72d450c5610a8f5b09ad52
shabnamfarooq <shabnam@homeeasy.com>
2024-07-17 10:47:07 -0700

    Your commit message here

--- config.py
+++ config.py
@@ -4,15 +4,8 @@ from dotenv import load_dotenv
 # Load environment variables from .env file (for local development)
 load_dotenv()
 
-# Get the database path from environment variable or default based on environment
-DATABASE = os.getenv('SQLITE_DATABASE_URL')
-
-if DATABASE is None:
-    # Check if running in a deployment environment like Render
-    if os.getenv('RENDER_EXTERNAL_HOSTNAME'):
-        DATABASE = '/mnt/data/My_database.sqlite'
-    else:
-        DATABASE = 'My_database.sqlite'
+# Get the database path from environment variable or default to local file
+DATABASE = os.getenv('SQLITE_DATABASE_URL', 'My_database')
 
 def get_database_path():
     print(f"Using database path: {DATABASE}")  # Log the database path for verification
